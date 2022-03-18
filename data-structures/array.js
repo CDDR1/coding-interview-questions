74da2c0f2417aa51407556afb9a56a0ff962f7ba
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-03-17 20:52:27 -0400

    Adding implementation of the Array Data Structure in JavaScript

---
+++ data-structures/array.js
@@ -0,0 +1,44 @@
+// Implementation of the array data structure using JavaScript
+// IMPORTANT: Arrays in JavaScript are just objects with integer-based keys that act like indexes.
+
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) {
+        const item = this.data[index];
+        this.shiftItems(index);
+        return this.data[this.data.length - 1];
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+  
+    len() {
+      return this.length;
+    }
+}
